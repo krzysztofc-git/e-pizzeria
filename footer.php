3a1203f7a0503ff53880471ac843ff3e69af9646
krzysztofc-git <118632157+krzysztofc-git@users.noreply.github.com>
2022-12-21 22:23:13 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <address>Please contact us at:<br> <a href="tel:1-800-EAT">1-800-EAT</a></address>   
+    <div class="social-links">
+        <img src="images/pizza-1-16.png" alt="logo">
+        <img src="images/pizza-1-16.png" alt="logo">
+        <img src="images/pizza-1-16.png" alt="logo">
+        <a href="https://icons8.com" style="font-size:10px; font-weight:bold; color:black; position:relative; bottom: 15px;">Icons by Icons8</a>
+        <img src="images/pizza-1-16.png" alt="logo">
+    </div>
+</footer>
