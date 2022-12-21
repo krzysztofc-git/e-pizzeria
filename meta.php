348d421d0823df62740218147a5e5be69fa225ad
krzysztofc-git <118632157+krzysztofc-git@users.noreply.github.com>
2022-12-21 22:27:04 +0100

    Create meta.php

---
+++ meta.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="author" content="me">
+<meta name="description" content="e-pizzeria - reserve your pizza online">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+
+<link rel="stylesheet" href="style.css">
